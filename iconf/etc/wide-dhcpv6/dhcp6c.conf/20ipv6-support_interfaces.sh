c5e7d9cd28c1d4ecc4277a14cb04b258907d2828
Felix Jacobi <felix.jacobi@stsbl.de>
2017-10-22 22:52:01 +0200

    Improved wide-dhcpv6 config

--- iconf/etc/wide-dhcpv6/dhcp6c.conf/20ipv6-support_interfaces.sh
+++ iconf/etc/wide-dhcpv6/dhcp6c.conf/20ipv6-support_interfaces.sh
@@ -3,24 +3,29 @@
 dhcp_interfaces=0
 for i in $(netquery6 --format nic | uniq | grep "$(cat /var/lib/iserv/config/ipv6-dhcp-interfaces.list)")
 do
-  echo "        interface $i {"
-  echo "                send ia-pd 0;"
-  echo "                send rapid-commit;"
-  echo "        };"
+  echo "interface $i {"
+  echo "        send ia-pd 0;"
+  echo "        send rapid-commit;"
+  echo "};"
+  echo
   let dhcp_interfaces++
 done
 
 if [ $dhcp_interfaces -gt 0 ] && [[ $(netquery6 --format nic --lan | uniq | grep "$(cat /var/lib/iserv/config/ipv6-delegation-interfaces.list)" | wc -l) -gt 0 ]]
 then
-  echo "        id-assoc pd {"
+  echo "id-assoc pd 0 {"
+  echo "        prefix ::/64 infinity;"
+  echo
   id=0
   for i in $(netquery6 --format nic --lan | uniq | grep "$(cat /var/lib/iserv/config/ipv6-delegation-interfaces.list)")
   do
-    echo "                prefix-interface $i {"
+    echo "        prefix-interface $i {"
     let id++
-    echo "                        sla-id $id;"
-    echo "                        sla-len 2;"
-    echo "                };"
+    echo "                sla-id $id;"
+    echo "                sla-len 0;"
+    echo "        };"
+    echo
   done
-  echo "        };"
+  echo "};"
+  echo
 fi
