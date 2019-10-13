bcccd0b763c06cb3841d4bed1d7fd288b47b336b
Felix Jacobi <Felix@jacobi-hamburg.net>
2019-10-13 16:46:07 +0200

    sysctl: Fixed sysctl.conf templates for VLAN NICs

--- iconf/etc/sysctl.conf/50config-ipv6_tempaddr.sh
+++ iconf/etc/sysctl.conf/50config-ipv6_tempaddr.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+WanNics=($(netquery6 -wg nic | sort | uniq | sed 's#\.#/#g'))
+LanNics=($(netquery6 -gul nic | sort | uniq | sed 's#\.#/#g'))
+
 if [ ! -f "/var/lib/dpkg/info/iserv-backup.list" ] &&
     [ ! -f "/var/lib/dpkg/info/iserv-backup2-server.list" ] &&
-    [ -n "$(netquery6 -wg nic)" ]
+    [[ ${#WanNics[@]} -gt 0 ]]
 then
   echo "# Prefer tempaddr for wan interfaces"
-  for nic in $(netquery6 -wg nic)
+  for nic in ${WanNics[@]}
   do
     echo "net.ipv6.conf.$nic.use_tempaddr = 2"
   done
 fi
 
-[ -n "$(netquery6 -gl nic)" ] && echo "# Kill tempaddr for lan interfaces"
+if [[ ${#LanNics[@]} -gt 0 ]]
+then
+  echo "# Kill tempaddr for lan interfaces"
 
-for nic in $(netquery6 -gl nic)
-do
-  echo "net.ipv6.conf.$nic.use_tempaddr = 0"
-done
+  for nic in ${LanNics[@]}
+  do
+    echo "net.ipv6.conf.$nic.use_tempaddr = 0"
+  done
+fi
 
-if [ -n "$(netquery6 -wg nic)" ] &&
+if [[ ${#WanNics[@]} -gt 0 ]] &&
     ([ -f "/var/lib/dpkg/info/iserv-backup.list" ]] ||
     [ -f "/var/lib/dpkg/info/iserv-backup2-server.list" ])
 then
   echo "# Kill tempaddr as backupmon has problems with it"
-  for nic in $(netquery6 -wg nic)
+  for nic in ${WanNics[@]}
   do
     echo "net.ipv6.conf.$nic.use_tempaddr = 0"
   done
